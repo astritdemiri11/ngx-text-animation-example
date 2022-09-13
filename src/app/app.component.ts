c00b20b8b8a9469d7789668f6beee6dc959a41cf
astritdemiri11 <astritdemiri11@gmail.com>
2022-09-13 22:09:45 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { TextReadComponent } from 'ngx-text-animation';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'ngx-text-animation';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(TextReadComponent) textRead?: TextReadComponent;
+
+  ngAfterViewInit() {
+    timer(0).subscribe(() => {
+      if (this.textRead) {
+        this.textRead.start();
+      }
+    });
+  }
 }
